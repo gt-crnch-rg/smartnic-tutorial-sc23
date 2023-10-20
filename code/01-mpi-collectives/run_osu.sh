9fa12e200ec134c5e10ecc8d56ea197e55052030
Yong Qin <yongq@nvidia.com>
2023-10-20 15:03:46 -0700

    Added MPI collective instructions.

---
+++ code/01-mpi-collectives/run_osu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -p thor
+#SBATCH -w thor[001-004],thorbf3a[001-004],thorbf2a[001-004]
+#SBATCH -w 30:0
+#SBATCH --res sc23
+
+sh /global/home/groups/rdmaworkshop/offload_stack/run.sh bf3_ib ompi sep23 osu allgatherv 4 32 1 0 0 0
+sh /global/home/groups/rdmaworkshop/offload_stack/run.sh bf3_ib ucc  sep23 osu allgatherv 4 32 1 4 4 4
+sh /global/home/groups/rdmaworkshop/offload_stack/run.sh bf2_ib ompi sep23 osu allgatherv 4 32 1 0 0 0
+sh /global/home/groups/rdmaworkshop/offload_stack/run.sh bf2_ib ucc  sep23 osu allgatherv 4 32 1 4 4 4
