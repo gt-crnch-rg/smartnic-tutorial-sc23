79dd5862e1e1888a1b7796a688da46e32900f2b3
Muhammad Usman <muhammad.usman@bsc.es>
2023-11-01 21:22:22 -0700

    added ip address as commandline input for task d

--- code/04-openmp-offload-dpu/run_service.sh
+++ code/04-openmp-offload-dpu/run_service.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -l
+
 #SBATCH -p thor
-#SBATCH -w thorbf3a031
+#SBATCH -w thorbf3a030
 #SBATCH -o service-%j.out
 
-# the following lineis for task c
+# sbatch ./run_service.sh
+
+# the following line is for task c
 if [ -n "$LIB_" ]; then
 	echo "lib path: $LIB_"
 	export LD_LIBRARY_PATH=$LIB_:$LD_LIBRARY_PATH
@@ -11,5 +14,6 @@ fi
 MODULEPATH_=/global/home/groups/rdmaworkshop/doca-omp-service/modulefiles/
 
 module load doca $MODULEPATH_/doca-omp-service.aarch64
+
 doca-omp-service
 
--- code/04-openmp-offload-dpu/run_task.sh
+++ code/04-openmp-offload-dpu/run_task.sh
@@ -1,12 +1,15 @@
 #!/bin/bash -l
+
 #SBATCH -p thor
-#SBATCH -w thor031
+#SBATCH -w thor030
 #SBATCH -o task-%j.out
+
+# sbatch ./run_task.sh ./task_a/hello
 
 # the following line is for task c
 if [ -n "$LIB_" ]; then
 	echo "lib path: $LIB_"
-	echo "the path includes following libs...:"
+	echo "the path includes following libs:"
 	ls $LIB_/*.so
 	export LD_LIBRARY_PATH=$LIB_:$LD_LIBRARY_PATH
 fi
@@ -15,7 +18,7 @@ module load doca
 
 COMMAND_=$@
 
-echo "cmd: $COMMAND_"
+echo "cmd : $COMMAND_"
 
 $COMMAND_
 
